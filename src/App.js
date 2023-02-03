ad0eb5e0ec25e1229e1328c657c28f9e3fbd2ae5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-03 17:23:52 +0300

    finish the intro page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Article from "./component/home-page/article";
 import Navbar from "./component/navbar/navbar";
+import Intro from "./component/home-page/intro";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
       {/* <Article /> */}
     </div>
   );
---
+++ src/component/home-page/intro.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Turky from "../../images/turky.jpg";
+import "./intro.css";
+
+const Intro = () => {
+  return (
+    <>
+      <div className="intro">
+        <div className="i-cont">
+          <img src={Turky} className="i-bg" />
+          <h1 className="i-text">
+            PROPERTIES IN TURKEY BUT ONLY THE GOOD ONES
+          </h1>
+          <form>
+            <div class="row g-3 ms-5">
+              <div class="col-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Location"
+                  aria-label="First name"
+                />
+              </div>
+              <div class="col-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Type"
+                  aria-label="Last name"
+                />
+              </div>
+              <div class="col-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Rooms"
+                  aria-label="Last name"
+                />
+              </div>
+              <div class="col-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Price Range"
+                  aria-label="Last name"
+                />
+              </div>
+              <div class="col-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="or Ref number"
+                  aria-label="Last name"
+                />
+              </div>
+              <div class="col-auto">
+                <button type="submit" class="btn btn-primary">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Intro;
