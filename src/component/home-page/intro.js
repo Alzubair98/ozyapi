c429301ada77d25cc5192fad56d0499fd74be8fe
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-04 22:59:12 +0300

    add drop down menu

--- src/component/home-page/intro.js
+++ src/component/home-page/intro.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Turky from "../../images/turky.jpg";
+
 import "./intro.css";
 
 const Intro = () => {
+  const [selects, setSelects] = useState();
   return (
     <>
       <div className="intro">
@@ -14,36 +16,56 @@ const Intro = () => {
           <form>
             <div class="row g-3 ms-5">
               <div class="col-md-2">
-                <input
+                <select class="form-control">
+                  <option>type 1</option>
+                  <option>type 2</option>
+                  <option>type 3</option>
+                </select>
+                {/* <input
                   type="text"
                   class="form-control"
                   placeholder="Location"
                   aria-label="First name"
-                />
+                /> */}
               </div>
               <div class="col-md-2">
-                <input
+                <select class="form-control">
+                  <option>type 1</option>
+                  <option>type 2</option>
+                  <option>type 3</option>
+                </select>
+                {/* <input
                   type="text"
                   class="form-control"
                   placeholder="Type"
                   aria-label="Last name"
-                />
+                /> */}
               </div>
               <div class="col-md-2">
-                <input
+                <select class="form-control">
+                  <option>1+1 room</option>
+                  <option>2+1 room</option>
+                  <option>3+1 room</option>
+                </select>
+                {/* <input
                   type="text"
                   class="form-control"
                   placeholder="Rooms"
                   aria-label="Last name"
-                />
+                /> */}
               </div>
               <div class="col-md-2">
-                <input
+                <select class="form-control">
+                  <option>1+1 room</option>
+                  <option>2+1 room</option>
+                  <option>3+1 room</option>
+                </select>
+                {/* <input
                   type="text"
                   class="form-control"
                   placeholder="Price Range"
                   aria-label="Last name"
-                />
+                /> */}
               </div>
               <div class="col-md-2">
                 <input
