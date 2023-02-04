22f74636d26117848f4441e83f9b96600f2ea119
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-04 11:21:18 +0300

    add second chapter for the articles

--- src/component/articles/article.js
+++ src/component/articles/article.js
@@ -39,6 +39,39 @@ const Article = () => {
             <img src="/deals.jpg" alt="image" />
           </div>
         </div>
+        <div className="a-f-card">
+          <div className="a-f-image">
+            <img src="/deals.jpg" alt="image" />
+          </div>
+          <div className="a-f-text col-md-8">
+            <h1>Property for sale in Turkey</h1>
+            <p>
+              Congratulations ,you have just landed on #1 website to search for
+              // property for sale in Turkey. Capitol Estate is the go-to
+              authority // for foreigners buying property in Turkey. With a
+              decade of // experience, we specialize in overseas purchases for
+              clients in // Western Europe, Russia, and the Middle East. With
+              one of the // largest property portfolios in Turkey, we are
+              dedicated and able // to provide you with maximum choice and
+              premium quality.
+            </p>
+            <p>
+              Providing you with the best selection of Real Estate in Turkey,
+              the best return on investment, and the best customer service
+              possible are what drive us. Everything about Capitol Estate, from
+              our comprehensive and easy-to-use website, to our all inclusive
+              free viewing trips have been designed to give you the best home
+              buying experience possible.
+            </p>
+            <p>
+              Everything Capitol Estate does is for you… our client! We spend
+              countless hours mastering and learning new methods to secure you
+              the finest investments, regardless of the city you buy in, so that
+              you will enjoy the optimal positive cash flow income or return on
+              your investment.
+            </p>
+          </div>
+        </div>
       </div>
     </>
     // <>
