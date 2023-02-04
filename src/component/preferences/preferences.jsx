19927506ebb39fddad36ee47b3c3977be65666cd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-04 15:29:02 +0300

    add card with caursol

--- src/component/preferences/preferences.jsx
+++ src/component/preferences/preferences.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./preferencess.css";
 import Navbar from "../navbar/navbar";
+import Carousel from "react-bootstrap/Carousel";
 
 const Preferencess = () => {
   return (
@@ -10,8 +11,51 @@ const Preferencess = () => {
         <div className="p-firstPart col-4">
           <h1>why</h1>
         </div>
-        <div className="p-secondPart col-8">
-          <h1>hello</h1>
+        <div className="p-secondPart col-md-8">
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-6">
+                <Carousel fade>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 p-c-img"
+                      src="/img1.jpg"
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 p-c-img"
+                      src="/img4.webp"
+                      alt="Second slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 p-c-img"
+                      src="/img6.webp"
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
