ed56eed0a40ae176ec68e28ed52c11b1121421cc
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-07 13:25:47 +0300

    fix key bug

--- src/component/services/FCarsouelData.js
+++ src/component/services/FCarsouelData.js
@@ -1,29 +1,36 @@
 const carouseldata = [
   {
+    key: "1",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "2",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "3",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "4",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "5",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "6",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
   {
+    key: "7",
     image: "/img1.jpg",
     description: " description text to add later by the company",
   },
