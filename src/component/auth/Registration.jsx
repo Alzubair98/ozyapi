75cf8fa3d2b1076c86dc907b9b61d8f0450178bd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-12 15:47:50 +0300

    import css in registration

--- src/component/auth/Registration.jsx
+++ src/component/auth/Registration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../Home/home.css";
 
 const Rigstration = (props) => {
   const [user, setUser] = useState({
