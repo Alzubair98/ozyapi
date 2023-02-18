75084eeac9ad34520c5cc5c5abf89a2251e52e2b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-18 15:30:56 +0300

    fix card

--- src/component/home-page/search.jsx
+++ src/component/home-page/search.jsx
@@ -6,21 +6,19 @@ const Search = (props) => {
   const state = props.data;
   return (
     <div className="D-card">
-      <div className="p-secondPart">
-        {state.map((item) => (
-          <SearchCard
-            id={item.id}
-            type={item.house_type}
-            images={item.images_urls}
-            desc={item.description}
-            location={item.location}
-            rooms={item.rooms}
-            baths={item.bath_rooms}
-            size={item.size}
-            refrence={item.ref_number}
-          />
-        ))}
-      </div>
+      {state.map((item) => (
+        <SearchCard
+          id={item.id}
+          type={item.house_type}
+          images={item.images_urls}
+          desc={item.description}
+          location={item.location}
+          rooms={item.rooms}
+          baths={item.bath_rooms}
+          size={item.size}
+          refrence={item.ref_number}
+        />
+      ))}
     </div>
   );
 };
--- src/component/home-page/searchCard.jsx
+++ src/component/home-page/searchCard.jsx
@@ -2,53 +2,60 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { MdBedroomChild } from "react-icons/md";
 import { GiBathtub } from "react-icons/gi";
-import { BsHouseFill } from "react-icons/bs";
+import { BsHouseFill, BsFillKeyFill } from "react-icons/bs";
 import { ImLocation } from "react-icons/im";
 import "./intro.css";
 
 const SearchCard = (props) => {
   return (
-    <div key={props.id} className="card mb-3 shadow-lg rounded">
+    <div className="card mb-3 rounded shadow-lg" key={props.id}>
       <div className="row g-0">
-        <div className="col-md-5">
-          <Carousel fade className="c-c-img">
-            {props.images.map((image) => (
-              <Carousel.Item>
-                <img
-                  className="d-block w-100 p-c-img"
-                  src={image}
-                  alt="First slide"
-                />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </div>
-        {/* inforamtion  */}
-        <div className="col-md-7 s-card">
+        <Carousel fade className="c-c-img">
+          {props.images.map((image) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100 p-c-img"
+                src={image}
+                alt="First slide"
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+
+        <div className="col-md-6">
           <div className="card-body">
             <h5 className="card-title">{props.type}</h5>
             <p className="card-text">{props.desc}</p>
-            <p className="card-text ">
-              <ImLocation className="s-room-image s-location" />
-              {props.location}
-            </p>
-            <div className="s-c-cont">
-              <p className="s-c-paragraph">
-                <MdBedroomChild className="s-room-image" />
-                {props.rooms} Rooms
-              </p>
+            <div className="card-cont-cont">
+              <div className="card-cont">
+                <p className="card-text">
+                  <ImLocation className="card-icons" />
+                  {props.location}
+                </p>
+                <p className="card-text">
+                  <BsFillKeyFill className="card-icons" />
+                  Ref Number :{props.refrence}
+                </p>
+              </div>
 
-              <p className="s-c-paragraph">
-                <GiBathtub className="s-room-image" />
-                {props.baths} Bathrooms
-              </p>
-
-              <p className="s-c-paragraph">
-                <BsHouseFill className="s-room-image" />
-                {props.size}m House size
-              </p>
+              <div className="card-cont">
+                <p className="card-text">
+                  <MdBedroomChild className="card-icons" />
+                  {props.rooms} Rooms
+                </p>
+                <p className="card-text">
+                  <GiBathtub className="card-icons" /> {props.baths} Bathrooms
+                </p>
+                <p className="card-text">
+                  <BsHouseFill className="card-icons" /> house size
+                  {props.size}m
+                </p>
+              </div>
             </div>
-            <p className="card-text">Refrence Number {props.refrence}</p>
+            <div className="button-cont">
+              <button className="btn btn-primary">Contact</button>
+              <button className="btn btn-success">Add to wish list</button>
+            </div>
           </div>
         </div>
       </div>
