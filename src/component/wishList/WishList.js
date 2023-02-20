4c23bd884a5556bff8836eee35ec1d3d01191d0f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-20 19:41:08 +0300

    add wishlist card

--- src/component/wishList/WishList.js
+++ src/component/wishList/WishList.js
@@ -1,8 +1,31 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
+import SearchCard from "../home-page/searchCard";
 
 const WishList = () => {
-  return <h1>WishList</h1>;
+  let data = JSON.parse(sessionStorage.getItem("user_like"));
+
+  return (
+    <div className="D-card">
+      {data.map((single) =>
+        single.map((item) => (
+          <div className="card" key={single.id}>
+            <SearchCard
+              id={item.id}
+              type={item.house_type}
+              images={item.images_urls}
+              desc={item.description}
+              location={item.location}
+              rooms={item.rooms}
+              baths={item.bath_rooms}
+              size={item.size}
+              price={item.price}
+              refrence={item.ref_number}
+            />
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default WishList;
