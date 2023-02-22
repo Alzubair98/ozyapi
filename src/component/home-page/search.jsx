367295cef8946bb7004626fbc037eca2eabe8ede
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-22 20:49:10 +0300

    delete functin

--- src/component/home-page/search.jsx
+++ src/component/home-page/search.jsx
@@ -4,15 +4,7 @@ import "./intro.css";
 
 const Search = (props) => {
   const state = props.data;
-  // const SaveToSession = (item) => {
-  //   // const house = state.filter((item) => item.id === Number(event.target.id));
 
-  //   const sessionRecords =
-  //     JSON.parse(sessionStorage.getItem("user_like")) || [];
-
-  //   sessionRecords.push(item);
-  //   sessionStorage.setItem("user_like", JSON.stringify(sessionRecords));
-  // };
   return (
     <div className="D-card">
       {state.map((item) => (
