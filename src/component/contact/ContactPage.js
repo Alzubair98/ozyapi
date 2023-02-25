1dabf7381c57edc338bcf1183489cf20304c4d83
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-02-25 17:24:23 +0300

    add contact page with styling

--- src/component/contact/ContactPage.js
+++ src/component/contact/ContactPage.js
@@ -1,42 +1,46 @@
 import React from "react";
 
 const ContactPage = () => {
-  <div className="form-cont">
-    <form
-      action="https://formspree.io/f/xlezkrge"
-      method="post"
-      className="contact-form m-2 p-5 n-box2"
-    >
-      <h3 className="font-bold">Contact Us</h3>
-      <hr />
-      <input
-        name="user-name"
-        type="text"
-        className="form-control mb-2"
-        placeholder="Name"
-        required
-      />
-      <input
-        name="user-mail"
-        type="text"
-        className="form-control mb-2"
-        placeholder="email"
-        required
-      />
-      <textarea
-        name="text"
-        type="text"
-        className="form-control mb-2"
-        rows={3}
-        defaultValue={"Write Your Message Here "}
-        required
-      />
+  return (
+    <div className="c-p-form container">
+      <div className="c-p-form-cont card c-p-card shadow-lg rounded">
+        <form
+          action="https://formspree.io/f/xlezkrge"
+          method="post"
+          className="c-p-contact-form m-2 "
+        >
+          <h3 className="font-bold">Contact Us</h3>
+          <hr />
+          <input
+            name="user-name"
+            type="text"
+            className="form-control mb-2"
+            placeholder="Name"
+            required
+          />
+          <input
+            name="user-mail"
+            type="text"
+            className="form-control mb-2"
+            placeholder="email"
+            required
+          />
+          <textarea
+            name="text"
+            type="text"
+            className="form-control mb-2"
+            rows={3}
+            defaultValue={"Write Your Message Here "}
+            required
+          />
 
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  </div>;
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -60,7 +60,7 @@ const Navbar = (props) => {
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="/Contact">
                     Contact
                   </a>
                 </li>
