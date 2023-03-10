870639afac376a1c00420814cbd60dd78d1810e0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-10 16:01:06 +0300

    add translatıon

--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./Redux/configureStore";
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
 import { Provider } from "react-redux";
 
+i18n.init({
+  interpolation: { escapeValue: false }, // React already does escaping
+  lng: "en", // language to use
+  resources: {
+    en: {
+      translation: {
+        Home: "Home",
+        Properties: "Properties",
+      },
+    },
+    ar: {
+      translation: {
+        Home: "الرئيسية",
+        Properties: "العروض",
+      },
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </Provider>
   </React.StrictMode>
 );
