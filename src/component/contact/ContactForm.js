91b4252cdd787c6bef90f1423d248e9ce77aa15f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-20 20:30:32 +0300

    contact translate

--- src/component/contact/ContactForm.js
+++ src/component/contact/ContactForm.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./Contact.css";
 
 const ContactForm = () => {
+  const { t } = useTranslation();
   return (
     <div className="form-cont">
       <form
@@ -9,20 +11,20 @@ const ContactForm = () => {
         method="post"
         className="contact-form m-2 p-5 n-box2"
       >
-        <h3 className="font-bold">Contact Us</h3>
+        <h3 className="font-bold">{t("contact")}</h3>
         <hr />
         <input
           name="user-name"
           type="text"
           className="form-control mb-2"
-          placeholder="Name"
+          placeholder={t("name")}
           required
         />
         <input
           name="user-mail"
           type="text"
           className="form-control mb-2"
-          placeholder="email"
+          placeholder={t("email")}
           required
         />
         <textarea
@@ -30,7 +32,7 @@ const ContactForm = () => {
           type="text"
           className="form-control mb-2"
           rows={3}
-          defaultValue={"Write Your Message Here "}
+          defaultValue={t("message")}
           required
         />
 
