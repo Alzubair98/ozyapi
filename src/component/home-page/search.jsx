3edd86c5b70e3c66d15bbf3ad2a15857d1f5d141
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-23 17:14:59 +0300

    add search bar form

--- src/component/home-page/search.jsx
+++ src/component/home-page/search.jsx
@@ -1,15 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchCard from "./searchCard";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import "./intro.css";
 
 const Search = (props) => {
   const state = props.data;
   const user = props.user.user;
 
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+  const [priceRange, setPriceRange] = useState("");
+  const [location, setLocation] = useState("");
+  const [rooms, setRooms] = useState("");
+  const [houseType, setHouseType] = useState("");
+  const [refNumber, setRefNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("test");
+    console.log("price", priceRange);
+    console.log("location", location);
+    console.log("rooms", rooms);
+    console.log("houseType", houseType);
+  };
+
   return (
     <div className="full-card">
       <div className="search-bar">
-        <h1>im here</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="g-3 ms-6 ps-3 pe-3 button-align">
+            <div className="">
+              <select
+                className="form-control"
+                id="location"
+                name="location"
+                onChange={(e) => setLocation(e.target.value)}
+              >
+                <option>{t("location")}</option>
+                <option value="istanbull">Istanbul</option>
+                <option value="adana">adana</option>
+                <option value="ankara">ankara</option>
+              </select>
+            </div>
+            <div className="pt-2">
+              <select
+                className="form-control"
+                id="house-type"
+                name="house-type"
+                onChange={(e) => setHouseType(e.target.value)}
+              >
+                <option>{t("type")}</option>
+                <option value="new house 3">new house 3</option>
+                <option value="normal house">normal house</option>
+                <option value="big house">big house</option>
+              </select>
+            </div>
+            <div className="pt-2">
+              <select
+                className="form-control"
+                id="rooms"
+                name="rooms"
+                onChange={(e) => setRooms(e.target.value)}
+              >
+                <option>{t("rooms")}</option>
+                <option value="2">1+1 room</option>
+                <option value="3">2+1 room</option>
+                <option value="4">3+1 room</option>
+                <option value="5">4+1 room</option>
+              </select>
+            </div>
+            <div className="pt-2">
+              <select
+                className="form-control"
+                id="location"
+                name="location"
+                onChange={(e) => setPriceRange(e.target.value)}
+              >
+                <option>{t("price")}</option>
+                <option value="1000-2000">1000-2000</option>
+                <option value="2000-3000">2000-3000</option>
+                <option value="3000-4000">3000-4000</option>
+                <option value="4000-5000">4000-5000</option>
+                <option value="5000-6000">5000-6000</option>
+              </select>
+            </div>
+            <div className="pt-2">
+              <input
+                value={refNumber}
+                name="ref_number"
+                type="text"
+                className="form-control"
+                placeholder={t("ref")}
+                aria-label="Last name"
+                onChange={(e) => setRefNumber(e.target.value)}
+              />
+            </div>
+            <div className="col-auto pt-2">
+              <button type="submit" className="btn btn-primary">
+                {t("search")}
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
       <div className="D-card">
         {state.map((item) => (
