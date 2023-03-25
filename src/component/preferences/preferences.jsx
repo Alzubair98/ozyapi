e25f5234fb8cbe4ff714b41e5975ce99db57cd6f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-25 16:30:35 +0300

    add alert in all properties

--- src/component/preferences/preferences.jsx
+++ src/component/preferences/preferences.jsx
@@ -13,23 +13,36 @@ const Preferencess = () => {
   const state = useSelector((state) => state.storeSlice);
   return (
     <>
-      <div className="P-card">
-        {state.map((item) => (
-          <SearchCard
-            item={item}
-            id={item.id}
-            type={item.house_type}
-            images={item.images_urls}
-            desc={item.description}
-            location={item.location}
-            rooms={item.rooms}
-            baths={item.bath_rooms}
-            size={item.size}
-            price={item.price}
-            refrence={item.ref_number}
-            mode={true}
-          />
-        ))}
+      <div className="all-card">
+        <div className="alert alert-success alert1 ">
+          <h4 className="alert-heading">All Properties!</h4>
+          <p>
+            Here you can see all Properties to search for spesific Propertiy
+            click the button below
+          </p>
+          <hr />
+          <a href="/search" className="btn btn-primary">
+            search
+          </a>
+        </div>
+        <div className="D-card P-card">
+          {state.map((item) => (
+            <SearchCard
+              item={item}
+              id={item.id}
+              type={item.house_type}
+              images={item.images_urls}
+              desc={item.description}
+              location={item.location}
+              rooms={item.rooms}
+              baths={item.bath_rooms}
+              size={item.size}
+              price={item.price}
+              refrence={item.ref_number}
+              mode={true}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
