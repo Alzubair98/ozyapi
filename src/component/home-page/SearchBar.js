57d3f050d79ddf0f260e83e51448d6dca9bc3aee
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-27 15:34:46 +0300

    add function

--- src/component/home-page/SearchBar.js
+++ src/component/home-page/SearchBar.js
@@ -10,6 +10,24 @@ const SearchBar = () => {
   const [rooms, setRooms] = useState("");
   const [houseType, setHouseType] = useState("");
   const [refNumber, setRefNumber] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios
+      .get("http://localhost:3001/search", {
+        params: {
+          location: location,
+          rooms: rooms,
+          house_type: houseType,
+          price: priceRange,
+          ref_number: refNumber,
+        },
+      })
+      .then((response) => {
+        props.onSearch(response.data);
+      })
+      .catch((error) => console.log("search", error));
+  };
 };
 
 export default SearchBar;
