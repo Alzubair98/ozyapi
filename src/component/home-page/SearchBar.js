3a02b4085c436d53182dd1ec1608eb33fe0bf5a2
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-27 15:45:09 +0300

    move search bar to a component

--- src/component/home-page/SearchBar.js
+++ src/component/home-page/SearchBar.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
-
-const SearchBar = () => {
+import "./intro.css";
+const SearchBar = (props) => {
   const { t } = useTranslation();
 
   const [priceRange, setPriceRange] = useState("");
@@ -28,6 +28,86 @@ const SearchBar = () => {
       })
       .catch((error) => console.log("search", error));
   };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <div className="g-3 ms-6 ps-3 pe-3">
+          <div className="">
+            <select
+              className="form-control"
+              id="location"
+              name="location"
+              onChange={(e) => setLocation(e.target.value)}
+            >
+              <option>{t("location")}</option>
+              <option value="istanbull">Istanbul</option>
+              <option value="adana">adana</option>
+              <option value="ankara">ankara</option>
+            </select>
+          </div>
+          <div className="pt-2">
+            <select
+              className="form-control"
+              id="house-type"
+              name="house-type"
+              onChange={(e) => setHouseType(e.target.value)}
+            >
+              <option>{t("type")}</option>
+              <option value="new house 3">new house 3</option>
+              <option value="normal house">normal house</option>
+              <option value="big house">big house</option>
+            </select>
+          </div>
+          <div className="pt-2">
+            <select
+              className="form-control"
+              id="rooms"
+              name="rooms"
+              onChange={(e) => setRooms(e.target.value)}
+            >
+              <option>{t("rooms")}</option>
+              <option value="2">1+1 room</option>
+              <option value="3">2+1 room</option>
+              <option value="4">3+1 room</option>
+              <option value="5">4+1 room</option>
+            </select>
+          </div>
+          <div className="pt-2">
+            <select
+              className="form-control"
+              id="location"
+              name="location"
+              onChange={(e) => setPriceRange(e.target.value)}
+            >
+              <option>{t("price")}</option>
+              <option value="1000-2000">1000-2000</option>
+              <option value="2000-3000">2000-3000</option>
+              <option value="3000-4000">3000-4000</option>
+              <option value="4000-5000">4000-5000</option>
+              <option value="5000-6000">5000-6000</option>
+            </select>
+          </div>
+          <div className="pt-2">
+            <input
+              value={refNumber}
+              name="ref_number"
+              type="text"
+              className="form-control"
+              placeholder={t("ref")}
+              aria-label="Last name"
+              onChange={(e) => setRefNumber(e.target.value)}
+            />
+          </div>
+          <div className="col-auto pt-2">
+            <button type="submit" className="btn btn-primary">
+              {t("search")}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/component/home-page/search.jsx
+++ src/component/home-page/search.jsx
@@ -3,6 +3,7 @@ import SearchCard from "./searchCard";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import "./intro.css";
+import SearchBar from "./SearchBar";
 
 const Search = (props) => {
   const state = props.data;
@@ -36,83 +37,7 @@ const Search = (props) => {
 
   return (
     <div className="full-card">
-      <div className="search-bar">
-        <form onSubmit={handleSubmit}>
-          <div className="g-3 ms-6 ps-3 pe-3">
-            <div className="">
-              <select
-                className="form-control"
-                id="location"
-                name="location"
-                onChange={(e) => setLocation(e.target.value)}
-              >
-                <option>{t("location")}</option>
-                <option value="istanbull">Istanbul</option>
-                <option value="adana">adana</option>
-                <option value="ankara">ankara</option>
-              </select>
-            </div>
-            <div className="pt-2">
-              <select
-                className="form-control"
-                id="house-type"
-                name="house-type"
-                onChange={(e) => setHouseType(e.target.value)}
-              >
-                <option>{t("type")}</option>
-                <option value="new house 3">new house 3</option>
-                <option value="normal house">normal house</option>
-                <option value="big house">big house</option>
-              </select>
-            </div>
-            <div className="pt-2">
-              <select
-                className="form-control"
-                id="rooms"
-                name="rooms"
-                onChange={(e) => setRooms(e.target.value)}
-              >
-                <option>{t("rooms")}</option>
-                <option value="2">1+1 room</option>
-                <option value="3">2+1 room</option>
-                <option value="4">3+1 room</option>
-                <option value="5">4+1 room</option>
-              </select>
-            </div>
-            <div className="pt-2">
-              <select
-                className="form-control"
-                id="location"
-                name="location"
-                onChange={(e) => setPriceRange(e.target.value)}
-              >
-                <option>{t("price")}</option>
-                <option value="1000-2000">1000-2000</option>
-                <option value="2000-3000">2000-3000</option>
-                <option value="3000-4000">3000-4000</option>
-                <option value="4000-5000">4000-5000</option>
-                <option value="5000-6000">5000-6000</option>
-              </select>
-            </div>
-            <div className="pt-2">
-              <input
-                value={refNumber}
-                name="ref_number"
-                type="text"
-                className="form-control"
-                placeholder={t("ref")}
-                aria-label="Last name"
-                onChange={(e) => setRefNumber(e.target.value)}
-              />
-            </div>
-            <div className="col-auto pt-2">
-              <button type="submit" className="btn btn-primary">
-                {t("search")}
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+      <SearchBar onSearch={props.onSearch} />
       <div className="D-card">
         {state.map((item) => (
           <SearchCard
